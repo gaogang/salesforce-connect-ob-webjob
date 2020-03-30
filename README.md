854b49937cd0138710ee5220927a0a2549a21ee3
Gang Gao <ayi7456@hotmail.com>
2020-03-30 22:46:50 +0100

    Removed cometd client and use jsforce instead

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 A simple javascript code which captures Salesforce data changes in a cosmos db. 
 
+## dependencies
+
+1. jsforce 
+2. CometD NodeJs Client
+3. CometD
+
+## Key points
+
+1. 
+
--- main.js
+++ main.js
@@ -1,42 +1,25 @@
 (function() {
-    require('cometd-nodejs-client').adapt();
+    var jsforce = require('jsforce');
 
-    const lib = require('cometd');
-    const jsforce = require('jsforce');
     const url = 'https://um1.lightning.force.com/cometd/48.0/';
     const channel = '/data/ChangeEvents';
 
-    const cometd = new lib.CometD();
-    
-    const sfconn = new jsforce().Connection({
+    console.log('Setting up jsforce...');
+
+    const sfconn = new jsforce.Connection({
         oauth2: {
-            clientId: '',
-            clientSecret: '',
-            redirectUrl: ''
+            clientId: '<your client Id>',
+            clientSecret: '<your client secret>',
+            redirectUrl: '<your redirect url>'
         }
     });
 
+    console.log('Acquiring SF session Id...');
 
-    sfconn.login('username', 'password', function(err, userInfo) {
-        cometd.configure({
-            url: url,
-            appendMessageTypeToURL: false,
-            requestHeaders: {
-                Authorization: 'OAuth ' + sfconn.sessionId
-            }
-        });
-
-        cometd.handshake(function(h) {
-            if (h.successful) {
-                // subscribe to data changes
-                cometd.subscribe(channel, function(message) {
-                    const data = message.data;
-    
-                    console.log(data);
-                });
-            } else {
-                console.log('fail to establish connection to ' + url + ' ' + h.error);
-            }
+    sfconn.login('<your SF username>', '<your SF password>', function(err, userInfo) {
+        console.log('SF session id acquired: ' + sfconn.accessToken);
+        sfconn.streaming.topic(channel).subscribe(function(message) {
+            console.log(JSON.stringify(message));
         });
     });
 })();
